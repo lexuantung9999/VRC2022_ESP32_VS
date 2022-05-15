8816d6ffd38981b2d91a6980561f00cc68a2880c
lexuantung9999 <lexuantung19999@gmail.com>
2022-05-15 09:45:11 +0700

    add PS2 lib, example reading PS2

--- VRC2022_ESP32/src/main.cpp
+++ VRC2022_ESP32/src/main.cpp
@@ -2,22 +2,50 @@
 #include <EEB.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <main.h>
+#include <PS2X_lib.h>
 
 DCMotor     VRC_Motor;
 Servo_Motor VRC_Servo;
+PS2X        VRC_PS2;
+char PS2_text[100];
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   VRC_Motor.Init();
   VRC_Servo.Init();
+
+  //config ps2:
+  int err = -1;
+  for(int i=0; i<10; i++){
+    delay(100);
+    err = VRC_PS2. config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
+    Serial. print(".");
+    if(!err)  break;
+  }
+  Serial.println("Sucsessfully Connect PS2 Controller!");
+
 } 
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   //example control motor:
+  /*
   VRC_Motor.Run(1,2500,1);
   VRC_Servo.Angle(0,1);
+  */
+
+  //example reading ps2:
+  
+  VRC_PS2.read_gamepad(0, 0);
+  if(VRC_PS2.Button(PSB_L1)){
+    sprintf(PS2_text,"RY: %d RX: %d LY: %d LX: %d",VRC_PS2.Analog(PSS_RY),VRC_PS2.Analog(PSS_RX),VRC_PS2.Analog(PSS_LY),VRC_PS2.Analog(PSS_LX));
+    Serial.println(PS2_text);
+    delay(200);
+  }
+  
 
 }
 
---
+++ VRC2022_ESP32/src/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+//define pin to communicate with PS2 
+#define PS2_DAT 12 //MISO  19
+#define PS2_CMD 13 //MOSI  23
+#define PS2_SEL 15 //SS     5
+#define PS2_CLK 14 //SLK   18
+//#define pressures   true
+#define pressures   false
+//#define rumble      true
+#define rumble      false
+
+#endif
